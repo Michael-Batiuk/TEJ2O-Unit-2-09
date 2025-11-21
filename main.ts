e0d62da27f13c6c8b292d879376c92af3f83c61c
Michael-Batiuk <Michael.Batiuk@stu.ocsb.ca>
2025-11-20 20:26:07 -0500

    R, P, S

--- main.ts
+++ main.ts
@@ -1,8 +1,38 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Michael Batiuk
+ * Created on: Nov 2025
+ * This program plays rock, paper, scissors
 */
 
-basic.showString('Hello, World!')
+// variables
+let randomNumber: number = 0
+
+randomNumber = -1
+
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// shake
+input.onGesture(Gesture.Shake, function() {
+
+    randomNumber = randint(0, 2)
+// rock
+    if (randomNumber == 0) {
+        
+        basic.showIcon(IconNames.SmallSquare)
+    }
+
+// paper
+    if (randomNumber == 1) {
+
+        basic.showIcon(IconNames.Square)
+    }
+
+// scissors
+    if (randomNumber == 2) {
+
+        basic.showIcon(IconNames.Scissors)
+    }
+})
